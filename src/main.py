c8b0e2d570a753db46620a7e680482cc75d6ad16
ntdkk0511 <spr_0511@icloud.com>
2025-12-30 10:58:02 +0900

    Refactor edge detection and timing logic

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time
 
 def show_image(title, img, max_width=800, max_height=600):
     """
@@ -32,13 +33,24 @@ def to_grayscale(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     return gray
 
-def detect_edge(gray):
+def detect_edge_canny(gray):
     """
-    グレースケール画像からエッジ検出
+    グレースケール画像からエッジ検出（Canny）
     """
-    edge = cv2.Canny(gray,100,200)
-    return edge
+    
+    canny = cv2.Canny(gray,100,200)
+    return canny
 
+def detect_edge_sobel(gray):
+    """
+    グレースケール画像からエッジ検出（Sobel）
+    """
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
+
+    magnitude = cv2.magnitude(sobel_x, sobel_y)
+    sobel = cv2.convertScaleAbs(magnitude)
+    return sobel
 
 
 def main():
@@ -57,9 +69,25 @@ def main():
     gray = to_grayscale(img)
     show_image("Grayscale Image",gray)
 
-    #エッジ
-    edge = detect_edge(gray)
-    show_image("Edge",edge)
+    #Cannyエッジ検出（時間計測）
+    start = time.time() #現在の時間をfloat型の秒単位で取得
+    canny = detect_edge_canny(gray)
+    canny_time = time.time() - start #現在時間からstart時の差を計測
+
+    #Sobelエッジ検出（時間計測）
+    start = time.time()
+    sobel = detect_edge_sobel(gray)
+    sobel_time = time.time() - start
+    
+    #結果表示
+    print(f"Sobel time: {sobel_time:.6f} sec")
+    print(f"Canny time: {canny_time:.6f} sec")
+    
+    #show_imageを計測に含まない！ユーザのキー入力待ち等を含むとアルゴリズムの処理時間ではない
+    show_image("Canny Edge",canny)
+    show_image("Sobel Edge",sobel)
+
+
 
 #このファイルを直接実行したときだけmain()を動かす文→大規模化に繋がる
 if __name__ == "__main__":
